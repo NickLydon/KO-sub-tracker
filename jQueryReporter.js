1946a194ac046a601aa8a3784e5d931a87844490
NickLydon <nickkell@gmail.com>
2014-03-20 00:02:01 +0000

    Removed redundant function
    Wasn't necessary to return a new function

--- jQueryReporter.js
+++ jQueryReporter.js
@@ -1,8 +1,5 @@
-function jQueryReporter() {
-	
-	return function(report, success, failure) {
-		$.post("localhost:3000", JSON.stringify(report))
-			.done(success)
-			.fail(failure);
-	};
+function jQueryReporter(report, success, failure) {	
+	$.post("localhost:3000", JSON.stringify(report))
+		.done(success)
+		.fail(failure);
 }
