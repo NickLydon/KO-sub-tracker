211913e4e2c0ad80172585b7ee596686d6b0256f
NickLydon <nickkell@gmail.com>
2014-03-23 18:47:59 +0000

    Choose port; First stab at a graph
    Port can be specified through command line argument, otherwise uses
    default value;
    Uses html5 canvas and chartjs to draw a line graph of the observable
    counts. Unfortunately there isn't an option to tag the lines with the
    observable's names, so they're listed to the right

--- server/server.js
+++ server/server.js
@@ -1,13 +1,111 @@
 var express = require('express'),
-	app = express();
-
-app.set('../views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-
+	app = express(),
+	_ = require('underscore'),
+	results = [
+		[{
+			name: 'observable1',
+			count: 5
+		},
+		{
+			name: 'observable2',
+			count: 5
+		}],
+		[{
+			name: 'observable1',
+			count: 6
+		},
+		{
+			name: 'observable2',
+			count: 5
+		}],
+		[{
+			name: 'observable1',
+			count: 9
+		},
+		{
+			name: 'observable2',
+			count: 5
+		}],
+		[{
+			name: 'observable1',
+			count: 11
+		},
+		{
+			name: 'observable2',
+			count: 5
+		}],
+		[{
+			name: 'observable1',
+			count: 15
+		},
+		{
+			name: 'observable2',
+			count: 5
+		}],
+		[{
+			name: 'observable1',
+			count: 30
+		},
+		{
+			name: 'observable2',
+			count: 5
+		}]
+	],
+	port = process.argv[2] || 3000;
+	
+app.set('view engine', 'jshtml');
+app.engine('jshtml', require('jshtml-express'));
 app.use('/javascript', express.static(__dirname + '/javascript'));
 
 app.get('/', function(req, res){
-  res.render('index.html');
+	var labels = _.range(0, results.length),
+		datasets = (function() {
+			var flattened = _.flatten(results);
+			var nameGroups = _.groupBy(flattened, 'name');
+			var keys = _.keys(nameGroups);
+			var totalColours = 256;
+			
+			var numberOfLines = keys.length;
+			
+			var numberOfdifferentColours = Math.ceil(totalColours / (numberOfLines + 1));
+			var lines = _.range(0, numberOfLines);
+			var lineColours = _.map(lines, function(x) {
+				return numberOfdifferentColours * x;
+			});
+			var datas = _.map(keys, function(k) {
+				
+				return {
+					name: k,
+					data: _.pluck(nameGroups[k], 'count')
+				};
+			});
+			var dataWithColours = _.zip(datas,lineColours);
+			
+			return _.map(dataWithColours, function(m) {
+				var data = m[0], colour = m[1],
+					makeColourString = function(a) {
+						return "rgba(" + colour + "," + colour + "," + colour + "," + a + ")";
+					},
+					strokeColour = makeColourString(1);
+				return {
+					name: data.name,
+					data: data.data,
+					fillColor : makeColourString(0.5),
+					strokecolor : strokeColour,
+					pointcolor : strokeColour,
+					pointstrokecolor : "#fff"
+				};
+			});
+		}());
+		  		  
+	res.locals({
+        data: JSON.stringify({
+			labels: labels,
+			datasets: datasets
+		})
+    });
+
+    res.render('index');
 });
 
 app.post('/', function(req, res) {
@@ -17,6 +115,6 @@ app.post('/', function(req, res) {
 	req.on('end', function () { console.log(JSON.parse(body)); });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
     console.log('Listening on port %d', server.address().port);
 });
